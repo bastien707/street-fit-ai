b089665cb1464f754441e30d67e73123e4d459ea
bastien707 <bastien.facqueur@efrei.net>
2023-01-07 13:09:46 +0100

    fix: catch exception

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,17 +7,22 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateAction = async (req, res) => {
-
+  try {
     const baseCompletion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${req.body.prompt}\n`,
-        temperature: 0.7,
-        max_tokens: 2048,
-      });
+      model: "text-davinci-003",
+      prompt: `${req.body.prompt}\n`,
+      temperature: 0.7,
+      max_tokens: 2048,
+    });
 
-      const basePromptOutput = baseCompletion.data.choices.pop();
+    const basePromptOutput = baseCompletion.data.choices.pop();
 
-      res.status(200).json({output: basePromptOutput});
+    res.status(200).json({output: basePromptOutput});
+    
+  } catch (error) {
+      console.log(error);
+      res.status(500).json({error: error.message});
+  }
 };
 
 export default generateAction;
